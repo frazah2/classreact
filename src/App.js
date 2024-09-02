9aad8e89c7ab0b7eab2849e7bb8ce24adfafd814
frazah2 <tankionlinefraz@gmail.com>
2024-09-02 17:41:16 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+
+
+class Hello extends component{
+  constructor(props){
+    super(props)
+    this.state = {
+      number: 1,
+      text: 'Hello'
+    }
+  }
+ 
+  changeNumber = () => {
+    this.setState(
+      (prev) => ({
+        number: prev.number + 1,
+        text: "not Hello"
+      })
+    )
+  }
+
+
+
 }
 
-export default App;
+function App(){
+  const {number,text} = this.state
+  return
+} 
+
+
+
+
+export default App; 
